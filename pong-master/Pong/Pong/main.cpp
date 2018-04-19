3f8e2d4cf2814c80e610464ca859e73449763ca3
Cenze9 <henri.lehtimaki@hotmail.com>
2018-04-19 16:38:19 +0300

    hope this fixes stuff backwards

--- pong-master/Pong/Pong/main.cpp
+++ pong-master/Pong/Pong/main.cpp
@@ -31,222 +31,221 @@ game *Game = nullptr;
 
 int main(int argc, char* arg[])
 {
-	
-	const int fps = 60;
-	const int frameDelay = 1000 / fps;
 
-	Uint64 now = SDL_GetPerformanceCounter();
-	Uint64 last = 0;
+    const int fps = 60;
+    const int frameDelay = 1000 / fps;
 
-	Uint32 frameStart;
-	int frameTime;
+    Uint64 now = SDL_GetPerformanceCounter();
+    Uint64 last = 0;
 
-	EnetStart();
+    Uint32 frameStart;
+    int frameTime;
 
-	Game = new game();
-	Game->Init("Pong: 1 vs. 1");
+    EnetStart();
 
-	while (Game->Running())
-	{
-		frameStart = SDL_GetTicks();
+    Game = new game();
+    Game->Init("Pong: 1 vs. 1");
 
-		Game->HandleEvents();
+    while (Game->Running())
+    {
+        frameStart = SDL_GetTicks();
+
+        Game->HandleEvents();
         //Enet();
-		if (roundCounter == 2)
-		{
-			Enet();
-			roundCounter = 0;
-		}
-		X_PosCheck();
-		Game->Update(clientID, tempY);
-		Game->Render();
-		//printf("%d", clientID);
+        if (roundCounter == 2)
+        {
+            Enet();
+            roundCounter = 0;
+        }
+        X_PosCheck();
+        Game->Update(clientID, tempY);
+        Game->Render();
+        //printf("%d", clientID);
 
-		frameTime = SDL_GetTicks() - frameStart;
-		if (frameDelay > frameTime) 
-		{
-			SDL_Delay(frameDelay - frameTime);
-		}
-		roundCounter++;
-	}
-	
-	atexit(enet_deinitialize);
-	Game->Clean();
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
+        roundCounter++;
+    }
 
-	return 0;
+    atexit(enet_deinitialize);
+    Game->Clean();
+
+    return 0;
 }
 
 int EnetStart()
 {  //init
-	if (enet_initialize() != 0) {
-		fprintf(stderr, "An error occured while initializing ENet.\n");
-		return EXIT_FAILURE;
-	}
-	//Create a host using enet_host_create
-	client = enet_host_create(NULL, 1, 2, 57600 / 8, 14400 / 8);
+    if (enet_initialize() != 0) {
+        fprintf(stderr, "An error occured while initializing ENet.\n");
+        return EXIT_FAILURE;
+    }
+    //Create a host using enet_host_create
+    client = enet_host_create(NULL, 1, 2, 57600 / 8, 14400 / 8);
 
-	
-	if (client == NULL) {
-		fprintf(stderr, "An error occured while trying to create an ENet server host\n");
-		exit(EXIT_FAILURE);
-	}
 
-	enet_address_set_host(&address, "127.0.0.1");
-	address.port = 2317;
+    if (client == NULL) {
+        fprintf(stderr, "An error occured while trying to create an ENet server host\n");
+        exit(EXIT_FAILURE);
+    }
+    //172.31.16.26
+    enet_address_set_host(&address, "127.0.0.1");
+    address.port = 2317;
 
-	// Connect and user service
-	peer = enet_host_connect(client, &address, 0, 0);
+    // Connect and user service
+    peer = enet_host_connect(client, &address, 0, 0);
 
-	if (peer == NULL) {
-		fprintf(stderr, "No available peers for initializing an ENet connection");
-		exit(EXIT_FAILURE);
-	}
+    if (peer == NULL) {
+        fprintf(stderr, "No available peers for initializing an ENet connection");
+        exit(EXIT_FAILURE);
+    }
 
-	eventStatus = 1;
+    eventStatus = 1;
 
-	X_PosCheck();
+    X_PosCheck();
 
-	return 0;
+    return 0;
 }
 
-void Enet() 
+void Enet()
 {
-	SDL_Delay(1000/60);
-	int dataP[6];
+    SDL_Delay(1000 / 60);
+    int dataP[6];
 
 
-	// If we had some event that interested us
-	while ((eventStatus = enet_host_service(client, &event, 0)) > 0) {
-		switch (event.type) {
-		case ENET_EVENT_TYPE_CONNECT:
-		{
-			printf("(Client) We got a new connection from %x\n",
-				event.peer->address.host);
-			break;
-		}
-		case ENET_EVENT_TYPE_RECEIVE:
-			
-			
-			
-			//std::cout << "receive" << std::endl;
-			int* p = (int*)event.packet->data;
-			for (int i = 0; i < 6; i++)
-			{
-				dataP[i] = (int)p[i];
-			}
+    // If we had some event that interested us
+    while ((eventStatus = enet_host_service(client, &event, 0)) > 0) {
+        switch (event.type) {
+        case ENET_EVENT_TYPE_CONNECT:
+        {
+            printf("(Client) We got a new connection from %x\n",
+                event.peer->address.host);
+            break;
+        }
+        case ENET_EVENT_TYPE_RECEIVE:
 
-			for (int i = 0; i < 6; i++)
-			{
-				std::cout << dataP[i] << std::endl;
 
-			}
-			//std::cout << dataP[0] << std::endl;
-			//std::cout << p[0] << std::endl;
-			//if (clientID==1)
-			//{
-			//	Game->Player2->posY = dataP[1];
-			//}
-			//else if (clientID == 2)
-			//{
-			//	Game->Player2->posY = dataP[0];
-			//}
-			//Game->Player2->posY = dataP[1];
-			//std::cout << dataP[1] << std::endl;
 
-			Game->ballX = dataP[3];
-			Game->ballY = dataP[4];
+            //std::cout << "receive" << std::endl;
+            int* p = (int*)event.packet->data;
+            for (int i = 0; i < 6; i++)
+            {
+                dataP[i] = (int)p[i];
+            }
 
-			if (unIdentified)
-			{
-				
-				clientID = dataP[5];
-				unIdentified = false;
-				
-			}
+            for (int i = 0; i < 6; i++)
+            {
+                std::cout << dataP[i] << std::endl;
+
+            }
+            //std::cout << dataP[0] << std::endl;
+            //std::cout << p[0] << std::endl;
+            //if (clientID==1)
+            //{
+            //	Game->Player2->posY = dataP[1];
+            //}
+            //else if (clientID == 2)
+            //{
+            //	Game->Player2->posY = dataP[0];
+            //}
+            //Game->Player2->posY = dataP[1];
+            //std::cout << dataP[1] << std::endl;
+
+            Game->ballX = dataP[3];
+            Game->ballY = dataP[4];
+
+            if (unIdentified)
+            {
+
+                clientID = dataP[5];
+                unIdentified = false;
+
+            }
 
             //tempY = dataP[2];
-			/*
-			if (data[4] == 1) 
-			{
-				Game->Player1->posX = 15;
-			}
-			else
-			{
-				Game->Player1->posX = 700 - 25;
-			}*/
-			// Lets broadcast this message to all
-			// enet_host_broadcast(client, 0, event.packet);
-			/*
-			case ENET_EVENT_TYPE_DISCONNECT:
-			printf("(Client) %s disconnected.\n", event.peer->data);
+            /*
+            if (data[4] == 1)
+            {
+            Game->Player1->posX = 15;
+            }
+            else
+            {
+            Game->Player1->posX = 700 - 25;
+            }*/
+            // Lets broadcast this message to all
+            // enet_host_broadcast(client, 0, event.packet);
+            /*
+            case ENET_EVENT_TYPE_DISCONNECT:
+            printf("(Client) %s disconnected.\n", event.peer->data);
 
-			// Reset client's information
-			event.peer->data = NULL;
-			break;
-			*/
-			break;
-		}
-	}
-    
-	
-	//dataP[1] = Game->Player1->posY;
-	if (clientID == 1)
-	{
-        if (abs(dataP[2]) < 700) 
+            // Reset client's information
+            event.peer->data = NULL;
+            break;
+            */
+            break;
+        }
+    }
+
+
+    //dataP[1] = Game->Player1->posY;
+    if (clientID == 1)
+    {
+        if (abs(dataP[2]) < 700)
         {
-            
+
             tempY = dataP[2];
         }
-        dataP[1] = -200;
+
         dataP[1] = Game->Player1->posY;
-	}
-	else if (clientID == 2)
-	{
+    }
+    else if (clientID == 2)
+    {
         if (abs(dataP[1]) < 700)
         {
             tempY = dataP[1];
         }
-        dataP[1] = -200;
         dataP[2] = Game->Player1->posY;
-	}
+    }
 
-   //for (int i = 0; i < 6; i++)
-   //{
-   //    std::cout << "Data is this    " << dataP[i] << std::endl;
-   //
-   //}
-	//	
-	//if (Game->Player1->posY != tempY) 
-	//{
-		//tempY = Game->Player1->posY;
-	//dataP[1] = Game->Player1->posY;
+    //for (int i = 0; i < 6; i++)
+    //{
+    //    std::cout << "Data is this    " << dataP[i] << std::endl;
+    //
+    //}
+    //	
+    //if (Game->Player1->posY != tempY) 
+    //{
+    //tempY = Game->Player1->posY;
+    //dataP[1] = Game->Player1->posY;
 
-        ENetPacket *packet = enet_packet_create((void*)dataP, (sizeof(dataP)+1), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
-		enet_peer_send(peer, 0, packet);
-		std::cout << "I sent data" << std::endl;
+    ENetPacket *packet = enet_packet_create((void*)dataP, (sizeof(dataP) + 1), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
+    enet_peer_send(peer, 0, packet);
+    std::cout << "I sent data" << std::endl;
 
-        int temparry[6];
-       /* for (int i = 0; i < 6; i++)
-        {
-            temparry[i] = (int)packet[i].data;
-            std::cout << "I sent this   " << temparry[i] << std::endl;
-        }*/
-        
-	//}
+    int temparry[6];
+    /* for (int i = 0; i < 6; i++)
+    {
+    temparry[i] = (int)packet[i].data;
+    std::cout << "I sent this   " << temparry[i] << std::endl;
+    }*/
+
+    //}
 }
 
 void X_PosCheck()
 {
-	if (clientID == 1)
-	{
-		Game->Player1->posX = 15;
-		Game->Player2->posX = 775;
-	}
-	if (clientID == 2)
-	{
-		Game->Player2->posX = 15;
-		Game->Player1->posX = 775;
-	}
+    if (clientID == 1)
+    {
+        Game->Player1->posX = 15;
+        Game->Player2->posX = 775;
+    }
+    if (clientID == 2)
+    {
+        Game->Player2->posX = 15;
+        Game->Player1->posX = 775;
+    }
 }
 
 
@@ -261,77 +260,77 @@ void X_PosCheck()
 /*
 int main(int argc, char **argv)
 {
-	ENetAddress address;
-	ENetHost *client;
-	ENetPeer *peer;
-	char message[1024];
-	ENetEvent event;
-	int eventStatus;
+ENetAddress address;
+ENetHost *client;
+ENetPeer *peer;
+char message[1024];
+ENetEvent event;
+int eventStatus;
 
-	// a. Initialize enet
-	if (enet_initialize() != 0) {
-		fprintf(stderr, "An error occured while initializing ENet.\n");
-		return EXIT_FAILURE;
-	}
+// a. Initialize enet
+if (enet_initialize() != 0) {
+fprintf(stderr, "An error occured while initializing ENet.\n");
+return EXIT_FAILURE;
+}
 
-	atexit(enet_deinitialize);
+atexit(enet_deinitialize);
 
-	// b. Create a host using enet_host_create
-	client = enet_host_create(NULL, 1, 2, 57600 / 8, 14400 / 8);
+// b. Create a host using enet_host_create
+client = enet_host_create(NULL, 1, 2, 57600 / 8, 14400 / 8);
 
-	if (client == NULL) {
-		fprintf(stderr, "An error occured while trying to create an ENet server host\n");
-		exit(EXIT_FAILURE);
-	}
+if (client == NULL) {
+fprintf(stderr, "An error occured while trying to create an ENet server host\n");
+exit(EXIT_FAILURE);
+}
 
-	enet_address_set_host(&address, "localhost");
-	address.port = 1234;
+enet_address_set_host(&address, "localhost");
+address.port = 1234;
 
-	// c. Connect and user service
-	peer = enet_host_connect(client, &address, 2, 0);
+// c. Connect and user service
+peer = enet_host_connect(client, &address, 2, 0);
 
-	if (peer == NULL) {
-		fprintf(stderr, "No available peers for initializing an ENet connection");
-		exit(EXIT_FAILURE);
-	}
+if (peer == NULL) {
+fprintf(stderr, "No available peers for initializing an ENet connection");
+exit(EXIT_FAILURE);
+}
 
-	eventStatus = 1;
+eventStatus = 1;
 
-	while (1) {
-		eventStatus = enet_host_service(client, &event, 50000);
+while (1) {
+eventStatus = enet_host_service(client, &event, 50000);
 
-		// If we had some event that interested us
-		if (eventStatus > 0) {
-			switch (event.type) {
-			case ENET_EVENT_TYPE_CONNECT:
-			{
-				printf("(Client) We got a new connection from %x\n",
-					event.peer->address.host);
+// If we had some event that interested us
+if (eventStatus > 0) {
+switch (event.type) {
+case ENET_EVENT_TYPE_CONNECT:
+{
+printf("(Client) We got a new connection from %x\n",
+event.peer->address.host);
 
-				float data[2];
-				data[0] = 0;
-				data[1] = 0;
-				ENetPacket *packet = enet_packet_create((void*)data, sizeof(data), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
-				enet_peer_send(event.peer, 0, packet);
+float data[2];
+data[0] = 0;
+data[1] = 0;
+ENetPacket *packet = enet_packet_create((void*)data, sizeof(data), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
+enet_peer_send(event.peer, 0, packet);
 
-				break;
-			}
-			case ENET_EVENT_TYPE_RECEIVE:
-				printf("(Client) Message from server : %s\n", event.packet->data);
-				// Lets broadcast this message to all
-				// enet_host_broadcast(client, 0, event.packet);
-				enet_packet_destroy(event.packet);
-				break;
-				/*
-			case ENET_EVENT_TYPE_DISCONNECT:
-				printf("(Client) %s disconnected.\n", event.peer->data);
+break;
+}
+case ENET_EVENT_TYPE_RECEIVE:
+printf("(Client) Message from server : %s\n", event.packet->data);
+// Lets broadcast this message to all
+// enet_host_broadcast(client, 0, event.packet);
+enet_packet_destroy(event.packet);
+break;
+/*
+case ENET_EVENT_TYPE_DISCONNECT:
+printf("(Client) %s disconnected.\n", event.peer->data);
 
-				// Reset client's information
-				event.peer->data = NULL;
-				break;
-				
-			}
-		}
-	}
+// Reset client's information
+event.peer->data = NULL;
+break;
+
+}
+}
+}
 }
 */
--- pong-master/Pong/PongS/Source.cpp
+++ pong-master/Pong/PongS/Source.cpp
@@ -11,131 +11,140 @@
 #define SDL_main main
 // server.c
 
-struct object{
-	int Id; 
-	int Px=0; 
-	int Py=0;
-	ENetPeer* info;
+struct object {
+    int Id;
+    int Px = 0;
+    int Py = 0;
+    ENetPeer* info;
 
-	object(int i, int x, int y) :Id(i), Px(x), Py(y){}
+    object(int i, int x, int y) :Id(i), Px(x), Py(y) {}
 };
 
 
-int hits = 0;
+int p2y = 0;
 int PaddleW = 10;
 int PaddleH = 70;
-float bVelX = -2;
+float bVelX = -1;
 float bVelY = 0;
-
+static int p1y = 0;
 
 
 int CheckCollision(object paddle, object ball);
 
 int main(int argc, char **argv)
 {
-	ENetAddress address;
-	ENetHost *server;
-	ENetEvent event;
-	ENetPacket* packit;
-	int playernum=0;
-	int eventStatus;
-	std::vector<object> objects;
-	int data[6];
-	
-
-
-	// a. Initialize enet
-	if (enet_initialize() != 0) {
-		fprintf(stderr, "An error occured while initializing ENet.\n");
-		return EXIT_FAILURE;
-	}
+    ENetAddress address;
+    ENetHost *server;
+    ENetEvent event;
+    ENetPacket* packit;
+    int playernum = 0;
+    int eventStatus;
+    std::vector<object> objects;
+    int data[6];
 
 
 
-	// b. Create a host using enet_host_create
-	address.host = ENET_HOST_ANY;
-	address.port = 2317;
+    // a. Initialize enet
+    if (enet_initialize() != 0) {
+        fprintf(stderr, "An error occured while initializing ENet.\n");
+        return EXIT_FAILURE;
+    }
 
-	server = enet_host_create(&address, 3, 0, 0, 0);
-	
-	if (server == NULL) {
-		fprintf(stderr, "An error occured while trying to create an ENet server host\n");
-		exit(EXIT_FAILURE);
-	}
-	object Ball(playernum, 0, 0);
-	Ball.info = 0;
-	objects.push_back(Ball);
-	// c. Connect and user service
-	eventStatus = 1;
 
-	SDL_Rect pallo = {objects[0].Px,objects[0].Py,10,10};
 
-	
+    // b. Create a host using enet_host_create
+    address.host = ENET_HOST_ANY;
+    address.port = 2317;
+
+    server = enet_host_create(&address, 3, 0, 0, 0);
+
+    if (server == NULL) {
+        fprintf(stderr, "An error occured while trying to create an ENet server host\n");
+        exit(EXIT_FAILURE);
+    }
+    object Ball(playernum, 0, 0);
+    Ball.info = 0;
+    objects.push_back(Ball);
+    // c. Connect and user service
+    eventStatus = 1;
+
+    SDL_Rect pallo = { objects[0].Px,objects[0].Py,10,10 };
+
+
     data[0] = 0;
     printf("Ready %d", playernum, "\n");
-	while (1) {
+    while (1) {
 
         eventStatus = enet_host_service(server, &event, 1000);
 
-		// If we had some event that interested us
-		if (eventStatus > 0) {
-			switch (event.type) {
-			
-			case ENET_EVENT_TYPE_CONNECT:
-			{
-				printf("(Server) We got a new connection from %x\n",
-				event.peer->address.host);
-				object test(playernum, 0, 0);
-				test.info = event.peer;
-				objects.push_back(test);
-				playernum++;
-				break;
-			}
-			case ENET_EVENT_TYPE_RECEIVE:
-			{
+        // If we had some event that interested us
+        if (eventStatus > 0) {
+            switch (event.type) {
+
+            case ENET_EVENT_TYPE_CONNECT:
+            {
+                printf("(Server) We got a new connection from %x\n",
+                    event.peer->address.host);
+                object test(playernum, 0, 0);
+                test.info = event.peer;
+                objects.push_back(test);
+                playernum++;
+                break;
+            }
+            case ENET_EVENT_TYPE_RECEIVE:
+            {
 
 
-				int *p = (int*)event.packet->data;
-				
+                int *p = (int*)event.packet->data;
 
-				//memcpy
+
+                //memcpy
                 //only need [1] and [2] spots from players
-                if (abs((int)p[1]) < 700&& abs((int)p[1]) > -100)
+                if (abs((int)p[1]) < 700)
                 {
                     data[1] = (int)p[1];
                 }
-                if (abs((int)p[2]) < 700 && abs((int)p[1]) > -100)
+                if (abs((int)p[2]) < 700)
                 {
                     data[2] = (int)p[2];
                 }
 
 
-				break;
-			}
 
-			case ENET_EVENT_TYPE_DISCONNECT:
-			{
-				//printf("%s disconnected.\n", event.peer->data);
-				printf("someone left\n");
-				// Reset client's information
-				event.peer->data = NULL;
-				printf("%d", playernum, "\n");
-				playernum--;
-				printf("%d", playernum, "\n");
 
-				for (int i=0; i < objects.size(); i++)
-				{
-					if (objects[i].info == event.peer)
-					{
 
-					}
-				}
 
-				break;
-			}
+                p1y = (int)p[1];
 
-			}
-		}
+                p2y = (int)p[2];
+
+
+                break;
+            }
+
+            case ENET_EVENT_TYPE_DISCONNECT:
+            {
+                //printf("%s disconnected.\n", event.peer->data);
+                printf("someone left\n");
+                // Reset client's information
+                event.peer->data = NULL;
+                printf("%d", playernum, "\n");
+                playernum--;
+                printf("%d", playernum, "\n");
+
+                for (int i = 0; i < objects.size(); i++)
+                {
+                    if (objects[i].info == event.peer)
+                    {
+
+                    }
+                }
+
+                break;
+            }
+
+            }
+        }
 
 
 
@@ -184,21 +193,6 @@ int main(int argc, char **argv)
 
         }
 
-        if (hits > 5)
-        {
-            if (bVelX > 0)
-            {
-                bVelX++;
-                hits = 0;
-            }
-            else if (bVelX < 0)
-            {
-                bVelX--;
-                hits = 0;
-            }
-        }
-
-
 
         //  BALL LOCATION CHECK
 
@@ -207,20 +201,12 @@ int main(int argc, char **argv)
             data[0] = 3;
             objects[0].Px = 400;
             objects[0].Py = 350;
-            
-            bVelX = -1;
-            hits = 0;
-
         }
         else if (objects[0].Px >800)
         {
             data[0] = 4;
             objects[0].Px = 400;
             objects[0].Py = 350;
-
-            bVelX = -1;
-            hits = 0;
-
         }
 
         if (objects[0].Py < 0 && bVelY<0)
@@ -241,105 +227,105 @@ int main(int argc, char **argv)
 
 
 
-		data[3] = objects[0].Px;
-		data[4] = objects[0].Py;
-		data[5] = playernum;
-		
-		packit = enet_packet_create((void*)data, (sizeof(data)+1), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
-		enet_host_broadcast(server, 0, packit);
+        data[3] = objects[0].Px;
+        data[4] = objects[0].Py;
+        data[5] = playernum;
 
-	}
+        packit = enet_packet_create((void*)data, (sizeof(data) + 1), ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE);
+        enet_host_broadcast(server, 0, packit);
 
-	atexit(enet_deinitialize);
+    }
+
+    atexit(enet_deinitialize);
     return 0;
 }
 
 
 int CheckCollision(object paddle, object ball)
 {
-	int by0 = ball.Py;
-	int by1 = ball.Py + 10;
+    int by0 = ball.Py;
+    int by1 = ball.Py + 10;
     int bx0 = ball.Px;
     int bx1 = ball.Px + 10;
 
     int ppx = ball.Px + 5;
     int ppy = ball.Py + 5;
 
-	int ry0 = paddle.Py;
-	int ry1 = paddle.Py + 70;
+    int ry0 = paddle.Py;
+    int ry1 = paddle.Py + 70;
     int rx0 = paddle.Px;
     int rx1 = paddle.Px + 10;
 
     bool happened = false;
 
-    if(rx1 < 50) // Left Paddle (Player 1)
-    { 
-        if (rx1 >= bx0) 
+    if (rx1 < 50) // Left Paddle (Player 1)
+    {
+        if (rx1 >= bx0)
         {
-           
-            if (ry0 <= by1 && ry1 >= by0) 
+
+            if (ry0 <= by1 && ry1 >= by0)
             {
                 happened = true;
-                hits++;
             }
         }
     }
     if (rx0 > 750) // Right Paddle (Payer 2)
     {
-       
+
         if (rx0 <= bx1)
         {
-            
+
             if (ry0 <= by1 && ry1 >= by0)
             {
                 happened = true;
-                hits++;
+
+
             }
         }
     }
 
-/*
+    /*
     if(ppx > rx0 && ppx < rx1)
     {
-        if (ppy > ry0 && ppy < ry1) 
-        {
-            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        }
-    }
-*/
-
-
-
-   /* if (pp > 600) {
-        happened = (rx0 > pp);
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    }
-    else if (pp < 200) 
+    if (ppy > ry0 && ppy < ry1)
     {
-        happened = (rx0 > pp);
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
-*/
-	//bool happened = ((ry0 < by0 && by0 < ry1) || (ry0 < by1 && by1 < ry1) ));
+    }
+    */
 
-    
 
-	if (!happened) {
-		return 0;
-        
-	}
 
-	int mby = by0 + 5;
-	int mry = ry0 + 35;
-	float mid_distance = mry - mby;
-	float RACKET_HITBACK_MAXANGLE = 85.0f*M_PI / 180.0f;
-	float angle = RACKET_HITBACK_MAXANGLE * (mid_distance / (70/2.0f+5.0f));
+    /* if (pp > 600) {
+    happened = (rx0 > pp);
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+    else if (pp < 200)
+    {
+    happened = (rx0 > pp);
+    }
+    */
+    //bool happened = ((ry0 < by0 && by0 < ry1) || (ry0 < by1 && by1 < ry1) ));
 
-	bVelY = round(-sin(angle)); // Y increases as you go down, not up.
-	bVelX = (-bVelX);
+
+
+    if (!happened) {
+        return 0;
+
+    }
+
+    int mby = by0 + 5;
+    int mry = ry0 + 35;
+    float mid_distance = mry - mby;
+    float RACKET_HITBACK_MAXANGLE = 85.0f*M_PI / 180.0f;
+    float angle = RACKET_HITBACK_MAXANGLE * (mid_distance / (70 / 2.0f + 5.0f));
+
+    bVelY = round(-sin(angle)); // Y increases as you go down, not up.
+    bVelX = (-bVelX);
     //bVelX = bVelX < 0 ? cosf(angle) : -cosf(angle);
 
 
-	return 1;
+    return 1;
 
 
 }
